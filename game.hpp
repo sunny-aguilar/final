e97b28b859361bd3c87b9968c5d81eea66e857d6
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-04 12:14:43 -0800

    final

--- game.hpp
+++ game.hpp
@@ -18,6 +18,7 @@ public:
     ~Game();
 
 private:
+
 };
 
 #endif // GAME_HPP
---
+++ menu.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sandro Aguilar on 2019-03-04.
+//
+
+#ifndef FINAL_MENU_HPP
+#define FINAL_MENU_HPP
+
+#endif //FINAL_MENU_HPP
