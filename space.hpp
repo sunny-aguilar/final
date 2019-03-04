f22185cbd0939810bf63a8745bce5513b0c3141f
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-04 11:15:19 -0800

    final

--- space.hpp
+++ space.hpp
@@ -1,7 +1,16 @@
-//
-// Created by Sandro Aguilar on 2019-03-04.
-//
-
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            March 4, 2019
+** Description:     The Space class provides a player spaces for
+**                  which they can move through. The space class is
+**                  an abstract class from which other classes are
+**                  derived from. Space class contains virtual
+**                  functions.
+**                  s
+**                  s
+**                  a
+**                  f
+*********************************************************************/
 #ifndef SPACE_HPP
 #define SPACE_HPP
 
