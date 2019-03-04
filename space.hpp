2b8467cd9aaeb6d792a85ff4de148f762f1b5f90
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-04 11:50:26 -0800

    final

--- space.hpp
+++ space.hpp
@@ -5,11 +5,9 @@
 **                  which they can move through. The space class is
 **                  an abstract class from which other classes are
 **                  derived from. Space class contains virtual
-**                  functions.
-**                  s
-**                  s
-**                  a
-**                  f
+**                  functions. Each space has 4 space pointers are
+**                  used to point to other spaces and allow a player
+**                  to move about.
 *********************************************************************/
 #ifndef SPACE_HPP
 #define SPACE_HPP
