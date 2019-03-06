dd439df453a1005479b6a9e5a75ba2aecd0fb18c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-05 18:53:53 -0800

    final

--- myCubicle.hpp
+++ myCubicle.hpp
@@ -1,9 +1,9 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            March 4, 2019
-** Description:     The Space class provides a player spaces for
-**                  which they can move through. The space class is
-**                  an abstract class from which other classes are
+** Description:     MyCubicle class is derived from the space clasa
+**                  and it represents the space that is the players
+**                  own work area.
 **                  derived from. Space class contains virtual
 **                  functions. Each space has 4 space pointers are
 **                  used to point to other spaces and allow a player
